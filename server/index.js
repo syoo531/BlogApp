198d905a2bccdaa8c28b9fc3aefb45042f75548e
유승희 <syoo531@naver.com>
2023-11-05 13:36:30 +0900

    add origin

--- server/index.js
+++ server/index.js
@@ -17,16 +17,21 @@ connectDB(); //mongoDB 연결
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 // const __dirname = new URL(".", import.meta.url).pathname; // Resolve __dirname
 // app.use("/assets", express.static(path.join(__dirname, "public")));
 //app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 //allows cookies to be sent with the request with settings
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); //this need to be initialized before the routes
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://memoriesblog123.netlify.app"],
+    credentials: true,
+  })
+); //this need to be initialized before the routes
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); //needed for post req
 app.use(cookieParser()); //parse Cookie header on the req and give data as req.cookies, if secret was provided req.signedCookies
